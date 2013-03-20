c581a6ee610aa676508629a0f185450b8219af1c
ojash dahal <ojash@live.com>
2013-03-20 23:58:20 +0545

    some mistakes corrected regarding drop down menu

--- js/nav.js
+++ js/nav.js
@@ -1,30 +1,7 @@
 // JavaScript Document
-
-    <!--
-    $(function () {
-        
-        $('marquee').marquee('pointer').mouseover(function () {
-            $(this).trigger('stop');
-        }).mouseout(function () {
-            $(this).trigger('start');
-        }).mousemove(function (event) {
-            if ($(this).data('drag') == true) {
-                this.scrollLeft = $(this).data('scrollX') + ($(this).data('x') - event.clientX);
-            }
-        }).mousedown(function (event) {
-            $(this).data('drag', true).data('x', event.clientX).data('scrollX', this.scrollLeft);
-        }).mouseup(function () {
-            $(this).data('drag', false);
-        });
-    });
-    //-->
-
 $(document).ready(function()
 {
-	
-//$('#login input').click(function() {						  	 
-	//							 $(this).animate({width: "150px", opacity:"1"}, 500);
-		//							 });
+
 $('.subnav li a').mouseover(function() {
 									 $(this).stop().animate({textIndent: "30px"}, 500);
 									 });
@@ -40,7 +17,7 @@ $('.subnav li a').mouseout(function() {
 	});
 	
 	$('#nav li.first').mouseover(function(){
-									   $(this).find('.subnav').css('width','200px');
+									   $(this).find('.subnav').css('width','100px');
 									   
 									   $(this).find('.subnav').stop().animate({height: '170px'
 },'slow');
@@ -54,7 +31,7 @@ $('.subnav li a').mouseout(function() {
   });
 	
 	$('#nav li.second').mouseover(function(){
-									   $(this).find('.subnav').css('width','200px');
+									   $(this).find('.subnav').css('width','100px');
 									   
 									   $(this).find('.subnav').stop().animate({height: '190px'
 },'slow');
@@ -67,7 +44,7 @@ $('.subnav li a').mouseout(function() {
 									   
   });
 			$('#nav li.last').mouseover(function(){
-									   $(this).find('.subnav').css('width','200px');
+									   $(this).find('.subnav').css('width','100px');
 									   
 									   $(this).find('.subnav').stop().animate({height: '200px'
 },'slow');
@@ -80,7 +57,7 @@ $('.subnav li a').mouseout(function() {
 									   
   });					
 			$('#nav li.last2').mouseover(function(){
-									   $(this).find('.subnav').css('width','200px');
+									   $(this).find('.subnav').css('width','100px');
 									   
 									   $(this).find('.subnav').stop().animate({height: '170px'
 },'slow');
@@ -93,21 +70,7 @@ $('.subnav li a').mouseout(function() {
 									   
   });	
 	
-	$('input.username').focus(function(){
-	    
-	    var value = this.value;
-		var title = this.title;
-		
-		if (value == title) {
-			$(this).val("");
-		};
-	});
-	$('input.username').blur(function(){
-		if(!$(this).val().length){
-		    $(this).val(this.title);
-		};
-	});
-
+	
 $('#header_links a').click(function(){
 				 $(this).parrent().find('.hidden').slideDown('slow', function() {
   				 $('ul.hidden').show();
