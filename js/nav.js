3caedf650a860e0d33832ca0c7ca28156e890c89
ojash dahal <ojash@live.com>
2013-03-21 16:16:50 +0545

    lots of contents changes in pages
    however the css needs to be done

--- js/nav.js
+++ js/nav.js
@@ -9,31 +9,11 @@ $('.subnav li a').mouseout(function() {
 									 $(this).stop().animate({textIndent: "20px"}, 500);
 									 });
 
-	$('#images').innerfade({ 
-		speed: 'slow', 
-		timeout: 9000, 
-		type: 'sequence', 
-		containerheight: '230px'
-	});
-	
-	$('#nav li.first').mouseover(function(){
-									   $(this).find('.subnav').css('width','100px');
-									   
-									   $(this).find('.subnav').stop().animate({height: '170px'
-},'slow');
-							   });
-	$('#nav li.first').mouseout(function() {
-									   
-									   $(this).find('.subnav').stop().animate({ height: '0px'
-},'slow');
-									 
-									   
-  });
 	
 	$('#nav li.second').mouseover(function(){
-									   $(this).find('.subnav').css('width','100px');
+									   $(this).find('.subnav').css('width','150px');
 									   
-									   $(this).find('.subnav').stop().animate({height: '190px'
+									   $(this).find('.subnav').stop().animate({height: '185px'
 },'slow');
 							   });
 	$('#nav li.second').mouseout(function() {
@@ -43,38 +23,7 @@ $('.subnav li a').mouseout(function() {
 									 
 									   
   });
-			$('#nav li.last').mouseover(function(){
-									   $(this).find('.subnav').css('width','100px');
-									   
-									   $(this).find('.subnav').stop().animate({height: '200px'
-},'slow');
-							   });
-	$('#nav li.last').mouseout(function() {
-									   
-									   $(this).find('.subnav').stop().animate({ height: '0px'
-},'slow');
-									 
-									   
-  });					
-			$('#nav li.last2').mouseover(function(){
-									   $(this).find('.subnav').css('width','100px');
-									   
-									   $(this).find('.subnav').stop().animate({height: '170px'
-},'slow');
-							   });
-	$('#nav li.last2').mouseout(function() {
-									   
-									   $(this).find('.subnav').stop().animate({ height: '0px'
-},'slow');
-									 
-									   
-  });	
-	
-	
-$('#header_links a').click(function(){
-				 $(this).parrent().find('.hidden').slideDown('slow', function() {
-  				 $('ul.hidden').show();
+						
+			
 
-});
-});
 });
